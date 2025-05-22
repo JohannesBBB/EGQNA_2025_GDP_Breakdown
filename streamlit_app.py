080d4b96e2318c3295c78c6dc83e9d15c323ea8b
JohannesBBB <johannes.buck@ec.europa.eu>
2025-05-22 16:16:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.set_page_config(layout="wide")
 st.markdown("""
     <style>
     .my-plotly-container {
-        max-width: 90vw;
+        max-width: 70vw;
         margin-left: auto;
         margin-right: auto;
     }
