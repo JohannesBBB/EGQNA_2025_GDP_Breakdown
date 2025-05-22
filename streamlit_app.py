27f0925ab33cf26ddd0045440e48e3dfd97e895b
JohannesBBB <johannes.buck@ec.europa.eu>
2025-05-22 16:22:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May 22 11:56:34 2025
-
-@author: buckjoh
-"""
-
 import streamlit as st
 import plotly.graph_objs as go
 import numpy as np
 
-# Set wide page layout
-st.set_page_config(layout="wide")
-
-# Sample data (categories and values)
-categories = ['B1GQ', 'B1G', 'D21X31', 'A', 'BTE', 'C', 'F', 'GTI', 'J', 'K', 'L', 'M_N', 'OTQ', 'RTU']
-
+# Your data (already scaled in decimals)
 growth_t45 = np.array([
     0.000366, 0.003099, -0.025091, 0.005045,
     0.007375, 0.008180, 0.032566, -0.000515,
@@ -43,6 +31,8 @@ cont_12ms = np.array([
     0.001309, -0.000220
 ])
 
+categories = ['B1GQ','B1G','D21X31','A','BTE','C','F','GTI','J','K','L','M_N','OTQ','RTU']
+
 def create_grouped_stacked_figure():
     fig = go.Figure()
 
@@ -54,7 +44,10 @@ def create_grouped_stacked_figure():
         marker_color='blue',
         offsetgroup=0,
         legendgroup='T+45',
-        hovertemplate='%{y:.3f}%<extra></extra>'  # value only, no trace name or category repeat
+        hovertemplate=(
+            "<b>%{x}</b><br>" +
+            "T+45: %{y:.3f}%<extra></extra>"
+        )
     ))
 
     # T+65 bars
@@ -65,7 +58,10 @@ def create_grouped_stacked_figure():
         marker_color='green',
         offsetgroup=1,
         legendgroup='T+65',
-        hovertemplate='%{y:.3f}%<extra></extra>'
+        hovertemplate=(
+            "<b>%{x}</b><br>" +
+            "T+65: %{y:.3f}%<extra></extra>"
+        )
     ))
 
     # cont_8ms bars (stacked)
@@ -76,7 +72,10 @@ def create_grouped_stacked_figure():
         marker_color='orange',
         offsetgroup=2,
         legendgroup='Contribution',
-        hovertemplate='%{y:.3f} pps<extra></extra>'
+        hovertemplate=(
+            "<b>%{x}</b><br>" +
+            "Cont. 8 MS: %{y:.3f} pps<extra></extra>"
+        )
     ))
 
     # cont_12ms bars (stacked)
@@ -87,7 +86,10 @@ def create_grouped_stacked_figure():
         marker_color='red',
         offsetgroup=2,
         legendgroup='Contribution',
-        hovertemplate='%{y:.3f} pps<extra></extra>'
+        hovertemplate=(
+            "<b>%{x}</b><br>" +
+            "Cont. 12 MS: %{y:.3f} pps<extra></extra>"
+        )
     ))
 
     fig.update_layout(
@@ -99,21 +101,16 @@ def create_grouped_stacked_figure():
         xaxis_tickangle=-45,
         bargap=0.15,
         bargroupgap=0.1,
-        legend=dict(
-            orientation='h',
-            yanchor='top',
-            y=1.02,
-            xanchor='left',
-            x=0.05,
-            font=dict(size=12)
-        ),
+        legend=dict(orientation='h', yanchor='bottom', y=1.02, xanchor='right', x=1),
         xaxis=dict(
-            tickfont=dict(family='Arial', size=12, color='black', weight='bold')
-        ),
-        margin=dict(l=40, r=40, t=80, b=40),
+            tickfont=dict(family='Arial', size=12, color='black', weight='bold')  # bold x-axis labels
+        )
+    margin=dict(l=40, r=40, t=80, b=40),
     )
-
     return fig
+
+# --- Streamlit setup ---
+st.set_page_config(layout="wide")  # Use wide mode to get more width
 
 st.title("Interactive Plotly Chart with Menus and Tabs")
 
@@ -125,33 +122,9 @@ tabs = st.tabs(tab_names)
 for tab_name, tab in zip(tab_names, tabs):
     with tab:
         st.header(f"{option} - {tab_name}")
-
-        # For demonstration, only QoQ Growth Rate and Production EA has the detailed plot
         if option == "QoQ Growth Rate" and tab_name == "Production - EA":
             fig = create_grouped_stacked_figure()
         else:
-            # Placeholder boring plot
-            x = np.linspace(0, 10, 100)
-            y = x
-            hover_text = [f"x value: {xi:.2f}<br>y value: {yi:.2f}" for xi, yi in zip(x, y)]
             fig = go.Figure()
-            fig.add_trace(go.Scatter(
-                x=x,
-                y=y,
-                mode='lines+markers',
-                hovertext=hover_text,
-                hoverinfo='text',
-                line=dict(color='royalblue'),
-                marker=dict(size=6)
-            ))
-            fig.update_layout(
-                title=f"Placeholder chart for {option} - {tab_name}",
-                xaxis_title="x",
-                yaxis_title="f(x)",
-                hovermode='closest'
-            )
-
-        with st.container():
-            st.markdown('<div class="my-plotly-container">', unsafe_allow_html=True)
-            st.plotly_chart(fig, use_container_width=False, width=1100, key=f"{option}_{tab_name}")
-            st.markdown('</div>', unsafe_allow_html=True)
+            fig.update_layout(title="No data yet for this tab")
+        st.plotly_chart(fig, use_container_width=True, key=f"{option}_{tab_name}")
